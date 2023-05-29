655ccf6ebadc83ca10ca2b8e61f4047e9cb7eacc
PavelVorobyev77 <92383863+PavelVorobyev77@users.noreply.github.com>
2023-05-29 09:33:51 +0700

    Add files via upload
    fndzfndfnzdfndf

---
+++ kino3542346567457/newtheatre_films/index.php
@@ -0,0 +1,21 @@
+<?php
+	$mysqli = new mysqli('localhost','root','','kino');
+	if ($mysqli->connect_errno) {
+		echo "Не удалось подключиться к MySQL: " . mysqli_connect_error();
+		return false;
+	};
+	$mysqli->set_charset("utf8");
+	$method = $_SERVER['REQUEST_METHOD'];
+		if ($method == 'POST'){
+			$a=array();
+			$json = json_decode(file_get_contents('php://input'),true);
+			foreach ($json as $v){
+				$text="INSERT INTO `theatres_films`(`theatreID`, `filmID`) VALUES ('".$v['theatreID']."','".$v['filmID']."')";
+				$result=$mysqli->query($text);
+				$b=array('result'=>$result);
+				$a[]=$b;
+			};	
+			echo json_encode($a);
+			return;			
+		}
+?>
